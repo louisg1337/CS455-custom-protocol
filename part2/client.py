8cbcbd63cc366ae1c904c9b37c65feb6f89c249d
louisg1337 <louisgrassi1337@gmail.com>
2023-10-09 14:55:32 -0400

    Comments and documentation

--- part2/client.py
+++ part2/client.py
@@ -6,52 +6,65 @@ import time
 HOST = "csa3.bu.edu"
 PORT = 58069  
 
-
-
+# Connection set up phase function that handles 
+# getting the connection established and error checking
 def CSP():
+    # Initialization code to set up socket connection
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
 
     while True:
+        # Ask user for initial connection phase protocol info
         protocolPhase = input("Protocol phase: ")
         measurementType = input("rtt or tput: ")
         numProbes = input("Number of probes: ")
         messageSize = input("Message size in B: ")
-        serverDelay = input("Server delay (ms): ")
+        serverDelay = input("Server delay (ms): ")  
 
+        # Combine all user info and send over to the server
         message = protocolPhase + " " + measurementType + " " + numProbes + " " + messageSize + " " + serverDelay
         s.sendall(message.encode('utf-8'))
 
-        response = s.recv(1024).decode('utf-8')
+        # Response received from server
+        response = s.recv(35000).decode('utf-8')
         print("\n" + response + "\n")
         
+        # If good repsonse, send to measurement phase, if not, terminate
         if response == "200 OK: Ready":
             MP(s, measurementType, numProbes, messageSize, serverDelay)
         else:
             s.close()
             break
     
+# This is the mesaurement phase function for our TCP experiment. Client
+# gets sent into here if the server verified their request to begin. 
 def MP(s, measurementType, numProbes, messageSize, serverDelay):
+    # Variable initializations to get all data needed to send to server
     print("Begin measuring period for... " + measurementType + "\n")
     numProbes = int(numProbes)
     messageSize = int(messageSize)
     serverDelay = int(serverDelay)
     payload = 'b' * messageSize
     responses = []
+    
+    # Send numProbes amount of messages to the server
     for i in range(numProbes):
+        # Start the timer and send message 
         timeStart = time.time()
         message = "m " + str(i) + " " + payload
         s.sendall(message.encode('utf-8'))
         
+        # Wait and listen for the echo response back from the server
         while True:
-            data = s.recv(1024).decode('utf-8')
+            data = s.recv(35000).decode('utf-8')
             
             # Error handling
             if data == "404 ERROR: Invalid Measurement Message":
                 print("404 ERROR: Invalid Measurement Message")
                 s.close()
             
-            
+            # If we get back valid data, echo the message to the client
+            # and display the time it took to get to back to them
             if len(data) > 0: 
                 timeEnd = time.time()
                 timeDiff = (timeEnd - timeStart) * 1000
@@ -60,6 +73,7 @@ def MP(s, measurementType, numProbes, messageSize, serverDelay):
                 responses.append(timeDiff)
                 break
     
+    # Calculate and display Avg RTT or Avg TPUT
     avgRtt = (sum(responses) / numProbes) - serverDelay
     if measurementType == "rtt":
         print("Average RTT (ms): " + str(avgRtt) + "\n")
@@ -68,5 +82,6 @@ def MP(s, measurementType, numProbes, messageSize, serverDelay):
         print("Average Throughput (bps): " + str(throughput) + "\n")
         
     print("///////////////////////\n")
-    
+
+# Call connection startup phase to begin the client's search 
 CSP()
--- part2/server.py
+++ part2/server.py
@@ -4,14 +4,22 @@ import time
 HOST = "csa3.bu.edu"
 PORT = 58069  
 
+# Connection set up phase function that handles 
+# getting the connection established and error checking
 def CSP():
+    # Boilerplate code to set up the socket and start
+    # listening for potential user to connect
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
     print(f"Connected by {addr}")
+    
+    # If someone is connected, constantly listen for any
+    # data that might be sent.
     while True:
-        data = conn.recv(1024)
+        # Receive data and parse it to be used below
+        data = conn.recv(35000)
         decodedData = data.decode('utf-8')
         parsedData = decodedData.split()
         
@@ -23,7 +31,7 @@ def CSP():
                 conn.sendall("404 ERROR: Invalid Connection Setup Message".encode('utf-8'))
             break    
             
-        # Put parse data to variable name
+        # Put parsed data to variable name
         protocolPhase = parsedData[0]
         measurementType = parsedData[1]
         numProbes = parsedData[2]
@@ -49,24 +57,32 @@ def CSP():
             conn.sendall("200 OK: Ready".encode('utf-8'))
             MP(conn, numProbes, serverDelay)
 
+# This is the mesaurement phase function for our TCP experiment. User gets sent
+# into here if and only if they passed pre requisite checks for their setup connection.
 def MP(conn, numProbes, serverDelay):
-    # Last package read, 
+    # Last package read, use this to make sure packages sent in right order
     last = 0
     
     # Listen for data until number of probes is matched
     while last < int(numProbes) - 1:
-        data = conn.recv(1024)
-        if len(data) > 0:
+        # If received some data, then start the logic 
+        data = conn.recv(35000)
+        if len(data) > 0:         
+            # Server delay if needed
             time.sleep(int(serverDelay) / 1000)
-            decodedData = data.decode('utf-8')
-            parsedData = decodedData.split()
+            
+            # Decode and parse data to extract the sequence number
+            parsedData = data.decode('utf-8').split()
             seqNum = int(parsedData[1])
             print("Sequence Number: " + str(seqNum))
         
+            # Handle if packets are out of order
             if seqNum < last:
                 conn.sendall("404 ERROR: Invalid Measurement Message".encode('utf-8'))
-        
             last = seqNum
+            
+            # Echo message back to client
             conn.sendall(data)
 
+# Calls connection phase to start up server
 CSP()
